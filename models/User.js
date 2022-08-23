4b4341f655d86fadbf6a2cc6392d91e976be6300
agvan <agvandyke25@gmail.com>
2022-08-23 11:29:09 -0500

    added the username field

--- models/User.js
+++ models/User.js
@@ -1,2 +1,11 @@
 // require statement for Mongoose
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+});
