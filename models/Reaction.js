91d5e14c39571f40c1a0402c64a5e49573c40eb1
agvan <agvandyke25@gmail.com>
2022-08-23 13:49:56 -0500

    started reaction schema, added reactionId field, & added two comments

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,7 +1,18 @@
 // import statements
 
 //imports Mongoose into the schema
-const { Schema, Type } = require("mongoose");
+const { Schema, Type, Types } = require("mongoose");
 
 // imports the logic to format the data entered into the createdAt field
 const dateFormat = require("../utils/dateFormat");
+
+// the reaction schema is associated with the thought model & schema
+const reactionSchema = new Schema(
+    {
+        // sets a custom id to prevent confusion with the parent id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+    }
+)
