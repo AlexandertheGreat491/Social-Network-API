0e4edb5407ea6f7103b87558d2fc1ae408a1bddf
agvan <agvandyke25@gmail.com>
2022-08-23 12:46:47 -0500

    added import statements & two comments

---
+++ models/Thought.js
@@ -0,0 +1,4 @@
+//import statements
+const { Schema, model, Types } = require("mongoose");
+// imports the logic to format the data entered into the createdAt fields
+const dateFormat = require('../utils/dateFormat');
