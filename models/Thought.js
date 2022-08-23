0b863d395202fa63f893182ea37bdf9af21e0283
agvan <agvandyke25@gmail.com>
2022-08-23 13:20:25 -0500

    finished the Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -40,3 +40,8 @@ const thoughtSchema = new Schema({
 thoughtSchema.virtual("reactionCount").get(function () {
     return this.reactions.length;
 });
+
+const Thought = model("Thought", thoughtSchema);
+
+// exports the Thought model
+module.exports = Thought;
