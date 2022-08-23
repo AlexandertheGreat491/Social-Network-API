36b50c1efb277819fb37fe1a7e7f72fc6e6ccf81
agvan <agvandyke25@gmail.com>
2022-08-23 11:54:58 -0500

    created the friends field & added two comments

--- models/User.js
+++ models/User.js
@@ -15,16 +15,22 @@ const userSchema = new Schema({
     unique: true,
     required: true,
     match: [
-        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-        "Please enter a valid email address!"
+      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+      "Please enter a valid email address!",
     ],
   },
   // thoughts field
   // includes an array of _id values referencing the Thought model
   thoughts: [
     {
-        type: Schema.Types.ObjectId,
-        ref: "Thought"
-    }
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
   ],
+  // friends field
+  // includes an array of _id values referencing the User model(self-reference)
+  friends: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
