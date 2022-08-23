4f74ee6e7d0f0125753f9be54471383b9dc0e84a
agvan <agvandyke25@gmail.com>
2022-08-23 18:35:39 -0500

    added the getAllUsers method to the userController

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,2 +1,14 @@
 // imports the models
 const { User, Thought } = require("../models");
+
+const userController = {
+    // the getAllUsers method will get all the users present in the database
+    getAllUsers(req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+}
