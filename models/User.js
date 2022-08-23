8b07b765206760b426d2ab7cbc28cf617392bd28
agvan <agvandyke25@gmail.com>
2022-08-23 11:34:23 -0500

    added the email field

--- models/User.js
+++ models/User.js
@@ -2,10 +2,21 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
+  // username field
   username: {
     type: String,
     unique: true,
     required: true,
     trim: true,
+  },
+  // email field
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please enter a valid email address!"
+    ],
   },
 });
