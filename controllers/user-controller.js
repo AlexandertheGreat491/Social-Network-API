28cf60d17f8312db8de472fc01ee4ec813134c87
agvan <agvandyke25@gmail.com>
2022-08-23 18:43:03 -0500

    fixed indentations

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,21 +1,20 @@
 // imports the models
 const { User } = require("../models");
 
-
 const userController = {
   // the getAllUsers method will get all the users present in the database
   getAllUsers(req, res) {
     User.find({})
-    .populate({
+      .populate({
         path: "thoughts",
         select: "-_v",
-    })
-    .select("-_v")
-    .sort({_id: -1})
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => {
+      })
+      .select("-_v")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
         console.log(err);
         res.status(400).json(err);
-    })
+      });
   },
 };
