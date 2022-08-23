2d362fba1371e3d76d35fafb60a92c53800c2198
agvan <agvandyke25@gmail.com>
2022-08-23 12:24:38 -0500

    finished the User model

--- models/User.js
+++ models/User.js
@@ -49,4 +49,9 @@ const userSchema = new Schema(
 // retrieves the length of the user's friends array field on query
 userSchema.virtual("friendCount").get(function () {
     return this.friends.length;
-})
+});
+
+const User = model("User", userSchema);
+
+// exports the User model
+module.exports = User;
