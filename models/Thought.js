71096680fef3061d6a61d881efb5276a05f31b1d
agvan <agvandyke25@gmail.com>
2022-08-23 12:50:43 -0500

    added the thoughtText field to the thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -2,3 +2,19 @@
 const { Schema, model, Types } = require("mongoose");
 // imports the logic to format the data entered into the createdAt fields
 const dateFormat = require('../utils/dateFormat');
+
+
+//reaction schema will go here
+
+
+const thoughtSchema = new Schema(
+    
+        {
+            thoughtText: {
+                type: String,
+                required: true,
+                maxLength: 280
+            },
+        }
+    
+)
