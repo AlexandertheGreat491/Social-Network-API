3245b92990784523c4107d37df2b96eb3260a28a
agvan <agvandyke25@gmail.com>
2022-08-23 11:51:46 -0500

    created the thoughts field & added  two comments

--- models/User.js
+++ models/User.js
@@ -19,4 +19,12 @@ const userSchema = new Schema({
         "Please enter a valid email address!"
     ],
   },
+  // thoughts field
+  // includes an array of _id values referencing the Thought model
+  thoughts: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+    }
+  ],
 });
