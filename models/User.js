bd0549ed0edcdd955746d5f5a0aa22764d9191cb
agvan <agvandyke25@gmail.com>
2022-08-23 12:01:36 -0500

    fixed friends field and added toJSON

--- models/User.js
+++ models/User.js
@@ -1,36 +1,47 @@
 // require statement for Mongoose
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  // username field
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-  },
-  // email field
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    match: [
-      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
-      "Please enter a valid email address!",
+const userSchema = new Schema(
+  {
+    // username field
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    // email field
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Please enter a valid email address!",
+      ],
+    },
+    // thoughts field
+    // includes an array of _id values referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    // friends field
+    // includes an array of _id values referencing the User model(self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
   },
-  // thoughts field
-  // includes an array of _id values referencing the Thought model
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
     },
-  ],
-  // friends field
-  // includes an array of _id values referencing the User model(self-reference)
-  friends: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+    id: false,
+  }
+);
