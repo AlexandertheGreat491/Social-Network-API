78f49a3635612ec7bfb5c9699d72c6fd754eecb2
agvan <agvandyke25@gmail.com>
2022-08-23 14:10:25 -0500

    added the createdAt field to the schema & added twocomments

--- models/Reaction.js
+++ models/Reaction.js
@@ -7,25 +7,30 @@ const { Schema, Type, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
 // the reaction schema is associated with the thought model & schema
-const reactionSchema = new Schema(
-    {
-        // reactioinId field
-        // sets a custom id to prevent confusion with the parent id
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        // reactionBody field
-        // this field will have a maximum of 280 characters
-        reactionBody: {
-            type: String,
-            required: true,
-            maxLength: 280
-        },
-        // username field
-        username: {
-            type: String,
-            required: true
-        },
-    }
-)
+const reactionSchema = new Schema({
+  // reactioinId field
+  // sets a custom id to prevent confusion with the parent id
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  // reactionBody field
+  // this field will have a maximum of 280 characters
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  // username field
+  username: {
+    type: String,
+    required: true,
+  },
+  // createdAt field
+  // all reactions will be timestamped in a specific format on query
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  }
+});
