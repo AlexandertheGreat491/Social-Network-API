9a72458f5d67076f6e847531bba6c3b6ddf8ecf1
agvan <agvandyke25@gmail.com>
2022-08-23 13:15:27 -0500

    added reactions as an array of nested documents with the reactionSchema, added toJSON, & added two comments

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,37 @@
 //import statements
 const { Schema, model, Types } = require("mongoose");
 // imports the logic to format the data entered into the createdAt fields
-const dateFormat = require('../utils/dateFormat');
-
+const dateFormat = require("../utils/dateFormat");
 
 //reaction schema will go here
 
-
-const thoughtSchema = new Schema(
-    
-        {
-            // thoughtText field that has between 1 and 280 characters
-            thoughtText: {
-                type: String,
-                required: true,
-                maxLength: 280
-            },
-            // createdAt field that will have the current timestamp
-            createdAt: {
-                type: Date,
-                default: Date.now,
-                get: (createdAtVal) => dateFormat(createdAtVal),
-            },
-            username: {
-                type: String,
-                required: true,
-            },
-        }
-    
-)
+const thoughtSchema = new Schema({
+  // thoughtText field that has between 1 and 280 characters
+  thoughtText: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  // createdAt field that will have the current timestamp
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+  // username field
+  username: {
+    type: String,
+    required: true,
+  },
+  // array of nested documents created with the reactionSchema
+  // these are like replies
+  reactions: [reactionSchema],
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+}
+);
