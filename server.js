92f85ef7d5dd6fbfe6733cbf2689fe8222a36f11
agvan <agvandyke25@gmail.com>
2022-08-23 11:04:09 -0500

    added three comments & updated one comment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// imports the express and mongoose libraries
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -10,12 +11,14 @@ app.use(express.static('public'));
 
 //app.use(require('./routes'));
 
+// provides the mongoose connection and connects to the local database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
   useNewUrlParser: true,
   useUnifiedTopology: true 
 }); 
 
-// Use this to log mongo queries being executed!
+// logs mongo queries that are being executed
 mongoose.set('debug', true);
 
+// allows the mongo server to connect to the port locally
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
