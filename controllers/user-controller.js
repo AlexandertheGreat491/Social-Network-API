9e14c5bd6be46bf74505c06507f3950272363f5b
agvan <agvandyke25@gmail.com>
2022-08-23 18:42:23 -0500

    updated getAllUsers method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,14 +1,21 @@
 // imports the models
-const { User, Thought } = require("../models");
+const { User } = require("../models");
+
 
 const userController = {
-    // the getAllUsers method will get all the users present in the database
-    getAllUsers(req, res) {
-        User.find({})
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
-    },
-}
+  // the getAllUsers method will get all the users present in the database
+  getAllUsers(req, res) {
+    User.find({})
+    .populate({
+        path: "thoughts",
+        select: "-_v",
+    })
+    .select("-_v")
+    .sort({_id: -1})
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+  },
+};
