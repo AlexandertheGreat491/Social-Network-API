82819dd5a348132ce2aa627edcb0e9a7028a2a22
agvan <agvandyke25@gmail.com>
2022-08-24 11:02:44 -0500

    added getAllThoughts method and one comment

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,2 +1,23 @@
 // imports the Thought and User models
 const { Thought, User } = require("../models");
+
+const thoughtController = {
+    // gets all of the thoughts shared by users
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({
+            path: "reactions",
+            select: "-_v",
+        })
+        .populate({
+            path: "thoughts",
+            select: "-_v",
+        })
+        .select("-_v")
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+}
