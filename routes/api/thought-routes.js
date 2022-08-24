8eec4f22d2705ea1c24e253c0d8f4663fbc9edb1
agvan <agvandyke25@gmail.com>
2022-08-24 15:17:11 -0500

    fixed the indentations

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,52 +1,37 @@
 // imports the express library
-const router = require('express').Router();
+const router = require("express").Router();
 
 // imports the methods from the thought-controller
 const {
-    getAllThoughts,
-    addThought,
-    getThoughtById,
-    updateThought,
-    removeThought,
-    addReaction,
-    removeReaction
-} = require('../../controllers/thought-controller');
-const {route} = require('./user-routes');
+  getAllThoughts,
+  addThought,
+  getThoughtById,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+const { route } = require("./user-routes");
 
 // /api/thoughts
 // GET for all thoughts and POST for newly created thoughts
-router
-.route('/')
-.get(getAllThoughts)
+router.route("/").get(getAllThoughts);
 
 // /api/thoughts/:userId
-router
-.router('/:userId')
-.post(addThought)
+router.router("/:userId").post(addThought);
 
 // /api/thoughts/:thoughtId
 // route will GET thoughts by id, PUT (update) thoughts by id, and DELETE thoughts by id
-router
-.route('/:thoughtId')
-.get(getThoughtById)
-.put(updateThought)
+router.route("/:thoughtId").get(getThoughtById).put(updateThought);
 
 // /api/thoughts/:userId/:thoughtId
-router
-.route(':/userId/:thoughtId')
-.delete(removeThought)
+router.route(":/userId/:thoughtId").delete(removeThought);
 
 // /api/thoughts/:thoughtId/reactions
-router
-.route('/:thoughtId/reactions')
-.post(addReaction);
+router.route("/:thoughtId/reactions").post(addReaction);
 
 // /api/:thoughtId/reactions/:reactionId
-router
-.route(':/thoughtId/reactions/:reactionId')
-.delete(removeReaction)
+router.route(":/thoughtId/reactions/:reactionId").delete(removeReaction);
 
 // exports the thought routes
 module.exports = router;
-
-
