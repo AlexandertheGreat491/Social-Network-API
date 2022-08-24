33fb7f6b7991be5e1c2b46018ae42c5adcaf62a6
agvan <agvandyke25@gmail.com>
2022-08-24 15:21:11 -0500

    added several comments

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -11,13 +11,15 @@ const {
   addReaction,
   removeReaction,
 } = require("../../controllers/thought-controller");
-const { route } = require("./user-routes");
+// imports the user-routes
+const { router } = require("./user-routes");
 
 // /api/thoughts
 // GET for all thoughts and POST for newly created thoughts
 router.route("/").get(getAllThoughts);
 
 // /api/thoughts/:userId
+// POST route to add thoughts to a specific user
 router.router("/:userId").post(addThought);
 
 // /api/thoughts/:thoughtId
@@ -25,12 +27,15 @@ router.router("/:userId").post(addThought);
 router.route("/:thoughtId").get(getThoughtById).put(updateThought);
 
 // /api/thoughts/:userId/:thoughtId
+// DELETE route to remove a thought from a specific user
 router.route(":/userId/:thoughtId").delete(removeThought);
 
 // /api/thoughts/:thoughtId/reactions
+// POST route to add reactions to a specific thought
 router.route("/:thoughtId/reactions").post(addReaction);
 
 // /api/:thoughtId/reactions/:reactionId
+// DELETE route to remove a specific reaction from a particular thought
 router.route(":/thoughtId/reactions/:reactionId").delete(removeReaction);
 
 // exports the thought routes
